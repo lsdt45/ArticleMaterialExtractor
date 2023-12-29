55cf85d391ff1244d4cfaf6bb6da50228179a2ad
lsdt45 <lsdt45@outlook.com>
2023-12-29 17:41:06 +0800

    [feat]:重构首页

---
+++ src/renderer/components/BackButton.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="back-button__wrapper">
+		<n-tooltip trigger="hover" placement="bottom-end" :show-arrow="false">
+			<template #trigger>
+				<span class="iconfont icon-back" @click="back"></span>
+			</template>
+			返回上一级
+		</n-tooltip>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import router from '@renderer/router/index';
+
+	function back() {
+		router.back();
+	}
+</script>
+
+<style lang="scss">
+	.back-button__wrapper {
+		.icon-back {
+			font-size: $main-icon-size;
+			color: $main-color;
+		}
+	}
+</style>
--- src/renderer/views/Home.vue
+++ src/renderer/views/Home.vue
@@ -1,81 +1,37 @@
 <template>
 	<div class="home__wrapper">
-		<div class="home-top">
-			<article-search @get-list="getList"></article-search>
-			<div class="setting-btn__wrapper">
-				<n-tooltip trigger="hover" placement="bottom-end" :show-arrow="false">
-					<template #trigger>
-						<span class="iconfont icon-folderOpen" @click="openFolder"></span>
-					</template>
-					打开输出文件夹
-				</n-tooltip>
-				<n-tooltip trigger="hover" placement="bottom-end" :show-arrow="false">
-					<template #trigger>
-						<span class="iconfont icon-shezhi" @click="openSetting"></span>
-					</template>
-					设置
-				</n-tooltip>
-			</div>
+		<div class="home-entry-item__containner">
+			<home-entry-item v-for="item in homeEntryList" :key="item.title" :home-item="item" />
 		</div>
-		<article-result ref="articleResultRef"></article-result>
-		<setting-main ref="settingMainRef"></setting-main>
 	</div>
 </template>
 
 <script setup lang="ts">
-	import ArticleSearch from '@renderer/components/Article/ArticleSearch.vue';
-	import ArticleResult from '@renderer/components/Article/ArticleResult.vue';
-	import SettingMain from '@renderer/components/Setting/SettingMain.vue';
+	import HomeEntryItem from '@renderer/components/Home/HomeEntryItem.vue';
+	import type { HomeEntryItemType } from '@renderer/components/Home/HomeType';
 
-	const articleResultRef = ref<InstanceType<typeof ArticleResult>>();
-	const settingMainRef = ref<InstanceType<typeof SettingMain>>();
-	function getList() {
-		articleResultRef.value?.getList();
-	}
-
-	/**
-	 * @description: 打开输出文件夹
-	 */
-	function openFolder() {}
-
-	/**
-	 * @description: 打开设置
-	 */
-	function openSetting() {
-		settingMainRef.value?.openDialog();
-	}
+	const homeEntryList: HomeEntryItemType[] = [
+		{
+			icon: 'iconfont icon-article-extract',
+			title: '文章提取',
+			route: '/article_extract',
+		},
+		{
+			icon: 'iconfont icon-follow-list',
+			title: '关注列表',
+			route: '/author_follow',
+		},
+	];
 </script>
 
 <style lang="scss">
 	.home__wrapper {
-		padding: 20px;
-		.icon-shezhi,
-		.icon-folderOpen {
-			position: relative;
-			left: 1em;
-			flex: 1;
-			display: inline-block;
-			width: 23px;
-			height: 36px;
-			cursor: pointer;
-			&::before {
-				color: $main-color;
-				font-size: 23px;
-				position: absolute;
-			}
-		}
-		.icon-folderOpen {
-			margin-right: 0.5em;
-			&::before {
-				font-size: 22px;
-			}
-		}
-		.home-top {
-			display: flex;
-			justify-content: space-between;
-			.setting-btn__wrapper {
-				flex: 1;
-			}
+		height: calc(100vh - 40px);
+		padding: 2em 0 0 2em;
+		.home-entry-item__containner {
+			display: grid;
+			grid-template-columns: repeat(auto-fill, 220px);
+			grid-gap: 2em;
 		}
 	}
 </style>
